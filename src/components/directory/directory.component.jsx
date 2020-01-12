457966020fd54add82bcad486ce14e42e2fa2fca
Abhishek Muralikrishnan <54433137+AbhishekMuralikrishnan@users.noreply.github.com>
2020-01-12 13:43:32 -0700

    Adding header component

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,18 +14,21 @@ class Directory extends React.Component {
                 title: 'hats',
                 imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                 id: 1,
+                size: 'small',
                 linkUrl: "hats"
               },
               {
                 title: 'jackets',
                 imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                 id: 2,
+                size: 'small',
                 linkUrl: "jackets"
               },
               {
                 title: 'sneakers',
                 imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
                 id: 3,
+                size: 'small',
                 linkUrl: "sneakers"
               },
               {
